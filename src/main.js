6330c64f5661f5f95e63d6a8b216a0c79c5c9aec
Onigirix <148443977+Onigirix@users.noreply.github.com>
2024-08-03 14:33:47 +0200

    Fixed a typo in the animation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-//const { invoke } = window.__TAURI__.core;
+const { invoke } = window.__TAURI__.core;
 
 window.addEventListener("DOMContentLoaded", () => {
-  const openNavButton = document.getElementById("openNav");
-  const closeNavButton = document.getElementById("closeNav");
+  const openNavButton = document.querySelector("#openNav");
+  const closeNavButton = document.querySelector("#closeNav");
 
   openNavButton.addEventListener("click", openNav);
   closeNavButton.addEventListener("click", closeNav);
@@ -13,7 +13,6 @@ function openNav() {
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("sidenav").style.width = "0";
   document.body.style.backgroundColor = "white";
