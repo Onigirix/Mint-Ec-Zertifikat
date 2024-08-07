6900c2741dc9d065110d3784fb47a5ed196ec13a
Onigirix <148443977+Onigirix@users.noreply.github.com>
2024-08-07 08:34:21 +0200

    Added SQLite via sqlx

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,28 @@
+pub mod db;
+use std::sync::Mutex;
+use tauri::{Builder, Manager};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[derive(Default)]
+struct AppState {
+    first_name: String,
+    last_name: String,
+    full_name: String,
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    db::setup_db();
+    Builder::default()
+        .setup(|app| {
+            app.manage(Mutex::new(AppState::default()));
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
