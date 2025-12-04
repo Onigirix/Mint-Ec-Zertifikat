2c889dcb39a404a3202071b883e5744143c4d404
Onigirix <148443977+Onigirix@users.noreply.github.com>
2025-12-04 20:39:30 +0100

    chore: removed dev tools in dev build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -85,8 +85,7 @@ fn prevent_default() -> tauri::plugin::TauriPlugin<tauri::Wry> {
 fn prevent_default() -> tauri::plugin::TauriPlugin<tauri::Wry> {
     use tauri_plugin_prevent_default::Flags;
 
-    let mut builder = tauri_plugin_prevent_default::Builder::new()
-        .with_flags(Flags::all().difference(Flags::DEV_TOOLS));
+    let mut builder = tauri_plugin_prevent_default::Builder::new().with_flags(Flags::all());
 
     #[cfg(target_os = "windows")]
     {
