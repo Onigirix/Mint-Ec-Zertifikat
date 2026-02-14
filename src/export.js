3e8901b9bc8a27c8be882ffb8e6a8a9cd01025a7
Mxitermi <david.gallas@gallas-it.de>
2026-02-14 09:08:14 +0100

    feat: add snackbar functionality for user notifications

--- src/export.js
+++ src/export.js
@@ -1,4 +1,16 @@
 const invoke = window.__TAURI__.core.invoke;
+window.addSchool = function s(event) {
+Conseole.log("Snackbar called");
+
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
 
 
 document.getElementById("generatePdf").addEventListener("click", async () => {
@@ -7,3 +19,4 @@ document.getElementById("generatePdf").addEventListener("click", async () => {
 		await invoke("generate_pdf");
 	}
 });
+
